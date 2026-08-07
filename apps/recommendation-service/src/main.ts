f6f473aaba5ecff226747f8c341c4e2e736010c3
Haibt9702 <buihaibq9702@gmail.com>
2026-08-07 09:35:01 +0700

    recommendation system

--- apps/recommendation-service/src/main.ts
+++ apps/recommendation-service/src/main.ts
@@ -1,17 +1,13 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
-import * as path from 'path';
+import cookieParser from 'cookie-parser';
 
 const app = express();
-
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb', extended: true}));
+app.use(cookieParser());
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to recomendation-service!' });
+  res.send({ message: 'Welcome to recommendation-service!' });
 });
 
 const port = process.env.PORT || 6007;
--- apps/recommendation-service/webpack.config.js
+++ apps/recommendation-service/webpack.config.js
@@ -1,17 +1,23 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+const { join, resolve } = require('path');
 
 module.exports = {
   output: {
     path: join(__dirname, 'dist'),
   },
+  resolve: {
+      alias: {
+        "@packages": resolve(__dirname, "../../packages")
+      },
+      extensions: [".ts", ".js"],
+    },
   plugins: [
     new NxAppWebpackPlugin({
       target: 'node',
       compiler: 'tsc',
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
-      assets: ["./src/assets"],
+      assets: [],
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
